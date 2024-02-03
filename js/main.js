5e0e818c7f9ec94fac067d73ca058c2179be1dae
AleksandrDruk <aleksandr.druk.ru@gmail.com>
2024-02-04 02:22:12 +0300

    hw5
    v0.1

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      // Удаляем класс active у всех ссылок
+      navLinks.forEach(function (link) {
+        link.classList.remove('active');
+      });
+
+      // Добавляем класс active к текущей ссылке
+      link.classList.add('active');
+    });
+  });
+});
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
